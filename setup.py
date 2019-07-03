e72dc54544eaeec4a73dcbbe9221140af93a0317
Eric Purdy <eric@radai.com>
2019-07-02 18:38:08 -0700

    pin tensorflow version

--- setup.py
+++ setup.py
@@ -60,8 +60,8 @@ setup(
         'tqdm',
     ],
     extras_require={
-        'tensorflow': ['tensorflow>=1.13.1'],
-        'tensorflow_gpu': ['tensorflow-gpu>=1.13.1'],
+        'tensorflow': ['tensorflow==1.13.1'],
+        'tensorflow_gpu': ['tensorflow-gpu==1.13.1'],
         'tensorflow-hub': ['tensorflow-hub>=0.1.1'],
         'tests': [
             'absl-py',
