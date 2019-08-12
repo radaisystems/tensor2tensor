2e83e0e56d7d5664ba7f3a6f188d5b252a2afddc
Eric Purdy <eric@radai.com>
2019-08-12 16:34:33 -0700

    Remove tensorflow cpu version

--- setup.py
+++ setup.py
@@ -60,7 +60,6 @@ setup(
         'tqdm',
     ],
     extras_require={
-        'tensorflow': ['tensorflow==1.13.1'],
         'tensorflow_gpu': ['tensorflow-gpu==1.13.1'],
         'tensorflow-hub': ['tensorflow-hub>=0.1.1'],
         'tests': [
